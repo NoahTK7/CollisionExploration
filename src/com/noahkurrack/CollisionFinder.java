ff31dbf6f281a9a6fc9a3f2bf9026c13ab90fc61
Noah Kurrack <noahkurrack@gmail.com>
2018-05-19 19:11:07 -0400

    tidy up code

--- src/com/noahkurrack/CollisionFinder.java
+++ src/com/noahkurrack/CollisionFinder.java
@@ -20,32 +20,32 @@ public class CollisionFinder {
     private static ArrayList<Long> hashes = new ArrayList<>();
 
     //begin meaningful code execution
-    public static void findCollisions() {
-        //start timer
+    static void findCollisions() {
+        //starts timer
         long startTime = System.nanoTime();
 
-        //create object that contains algorithm, takes input to produce hashes
+        //creates object that contains algorithm, takes input to produce hashes
         CRC32 hasher = new CRC32();
 
         //add one default value (0) to array of outputs
         hashes.add(hasher.getValue());
 
         //loop until collision found, max 2^32-1 (same amount of possible hashes)
-        for (int i = 0; i<Integer.MAX_VALUE; i++) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
 
-            //generate random string (random sequence of characters) to input to hashing algorithm (length of 8 characters)
+            //generates random string (random sequence of characters) to input to hashing algorithm (length of 8 characters)
             String currentString = randomStringGenerator(8);
 
-            //input random string into CRC32 algorithm
+            //inputs random string into CRC32 algorithm
             hasher.update(currentString.getBytes());
 
-            //get output of CRC32 algorithm (the hash)
+            //gets output of CRC32 algorithm (the hash)
             long hash = hasher.getValue();
 
-            //display current progress to user
-            System.out.print("\rCurrent hash: "+hash+"\t(hash #"+(i+1)+")");
+            //displays current progress to user
+            System.out.print("\rCurrent hash: " + hash + "\t(hash #" + (i + 1) + ")");
 
-            //reset algorithm for next hash
+            //resets algorithm for next hash
             hasher.reset();
 
             //checks if collision found
@@ -59,7 +59,7 @@ public class CollisionFinder {
 
                 //finds first string that produces same hash as current string
                 //only executes after collision found (that means there is always two inputs that produce the given output, this code finds the first one)
-                int otherIndex = Collections.binarySearch(hashes, hash);;
+                int otherIndex = Collections.binarySearch(hashes, hash);
                 String firstString = strings.get(otherIndex);
 
                 //confirms outputs match for both inputs
@@ -69,9 +69,9 @@ public class CollisionFinder {
 
                 //displays information about the collision to record in a spreadsheet
                 System.out.println("\n------------------------\nCollision!");
-                System.out.println("Collided hash:\t"+currentString+" --> "+hash+"\n\t\t\t\t"+firstString+" --> "+confirmHash);
-                System.out.println("Number of attempts:\t"+(i+1)+", (First occurrence: "+(otherIndex+1)+")");
-                System.out.println("Time elapsed: "+((stopTime-startTime)/1000000)+" milliseconds");
+                System.out.println("Collided hash:\t" + currentString + " --> " + hash + "\n\t\t\t\t" + firstString + " --> " + confirmHash);
+                System.out.println("Number of attempts:\t" + (i + 1) + ", (First occurrence: " + (otherIndex + 1) + ")");
+                System.out.println("Time elapsed: " + ((stopTime - startTime) / 1000000) + " milliseconds");
                 System.out.println("------------------------");
 
                 //exit program
@@ -87,17 +87,17 @@ public class CollisionFinder {
     private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     //returns a string of random characters of a specified length
-    private static String randomStringGenerator(int size){
+    private static String randomStringGenerator(int size) {
         StringBuilder builder = new StringBuilder();
         while (size-- != 0) {
-            int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
             builder.append(ALPHA_NUMERIC_STRING.charAt(character));
         }
         return builder.toString();
 
     }
 
-    //checks if collision has occurred (if output has been produced before)
+    //checks if collision has occurred (i.e. if output has been produced before)
     //otherwise, inserts input and output into their respective lists
     private static boolean insertInOrder(long element, String string) {
         for (int i = 0; i < hashes.size(); i++) {
@@ -105,7 +105,7 @@ public class CollisionFinder {
                 //collision found
                 return false;
             }
-            if (element < hashes.get(i)){
+            if (element < hashes.get(i)) {
                 hashes.add(i, element);
                 strings.add(i, string);
                 return true;
